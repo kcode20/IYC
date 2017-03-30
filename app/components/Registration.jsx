963b9ac0732b0404d26d1c8a2844ce7607fb5f26
Khristian Brooks <kh.brooks02@gmail.com>
2017-03-30 19:17:50 -0400

    Finalized Registration Questions

--- app/components/Registration.jsx
+++ app/components/Registration.jsx
@@ -1,61 +1,113 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Select from 'react-select';
 
-export const Registration = () => (
-   <div>
-    <Navbar/>
-    <div className="center_div container">
-    	<h1> Register for IYC </h1>
-    	<form className='' onSubmit={evt => {
-    	  evt.preventDefault()
-    	  console.log("I submitted with: ", evt.target.name.value, evt.target.email.value, evt.target.phone.value, evt.target.gender.value, evt.target.church.value, evt.target.pastor.value, evt.target.youthleader.value)
-    	}}>
-    	<fieldset>
-    		<legend> Contact Information </legend> 
+export class Registration extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          auxilary: []
+        };
 
-    		<label> Name </label>
-    	   <input className="form-control" name="name" />
-    	   <br/>
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
-    	   <label> Email </label>
-    	   <input className="form-control" name="email"/>
-    	   <br/>
+    handleInputChange(event) {
+        this.setState({
+          auxilary: this.state.auxilary.concat(event.target.value)
+        });
+    }
 
-    	   <label> Phone Number </label>
-    	   <input className="form-control" name="phone" />
-    	   <br/>
+    render(){
+        return(
 
-    	   <label> Gender </label>
-    	   <br/>
-    	   <input type="radio" name="gender" value="male"/> Male  
-    	   <input type="radio" name="gender" value="female"/> Female
-    	   <br/><br/>
+               <div>
+                <Navbar/>
+                <div className="center_div container">
+                	<h1> Register for IYC </h1>
+                	<form className='' onSubmit={evt => {
+                	  evt.preventDefault()
+                	  console.log("I submitted with: ", evt.target.name.value, evt.target.email.value, evt.target.phone.value, evt.target.age.value, evt.target.gender.value, evt.target.church.value, evt.target.pastor.value, evt.target.youthleader.value, evt.target.auxilary.value)
+                      console.log("This is the state: ", this.state);
+                	}}>
+                	<fieldset>
+                		<legend> Contact Information </legend> 
 
-    	</fieldset>
-    	<fieldset>
-    		<legend> Church Information </legend> 
-    		
-    		<label> Church Name </label>
-    	   <input className="form-control" name="church" />
-    	   <small> Please List Your Branch Church </small>
-    	   <br/>
+                		<label> Name </label>
+                	   <input className="form-control" name="name" />
+                	   <br/>
 
-    	   <label> Pastor Name </label>
-    	   <input className="form-control" name="pastor"/>
-    	   <br/>
+                	   <label> Email </label>
+                	   <input className="form-control" name="email"/>
+                	   <br/>
 
-    	   <label> Youth Leader Name </label>
-    	   <input className="form-control" name="youthleader" />
-    	   <br/>
+                	   <label> Phone Number </label>
+                	   <input className="form-control" name="phone" />
+                	   <br/>
 
-    	</fieldset>
+                       <select name="age">
+                         <option value="0-12">0-12</option>
+                         <option value="13-17">13-17</option>
+                         <option value="18-25">18-25</option>
+                         <option value="25-39">25-39</option>
+                         <option value="40+">40+</option>
+                       </select>
 
-    	  <button className='btn btn-primary' type="submit" value="Signup" > Signup </button>
-    	
-    	</form>
-    </div>
-   </div>
-)
+                	   <label> Gender </label>
+                	   <br/>
+                	   <input type="radio" name="gender" value="male"/> Male  
+                	   <input type="radio" name="gender" value="female"/> Female
+                	   <br/><br/>
+
+                	</fieldset>
+                	<fieldset>
+                		<legend> Church Information </legend> 
+                		
+                		<label> Church Name </label>
+                        <small> (Please List Your Branch Church) </small>
+                        <input className="form-control" name="church" />
+                        <br/>
+
+                        <label> Pastor Name </label>
+                        <input className="form-control" name="pastor"/>
+                        <br/>
+
+                        <label> Youth Leader Name </label>
+                        <input className="form-control" name="youthleader" />
+                        <br/>
+
+                        <label>  In My Home Church I am... </label>
+                        <small> (Click CTRL or Command to Select Multiple Options) </small>
+                        <br/>
+
+
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Pastor"/> Pastor  
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Minister"/> Minister 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Musician"/> Musician 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Choir Director"/> Choir Director 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Choir Member"/> Choir Member 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Sunday School Superintendent"/> Sunday School Superintendent 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Sunday School Teacher"/> Sunday School Teacher 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Kitchen Volunteer"/> Kitchen Volunteer 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Usher"/> Usher 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Youth Leader"/> Youth Leader 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Church Volunteer"/> Church Volunteer 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="N/A"/> Not Working in My Local Church 
+                        <input name="auxilary" onChange={this.handleInputChange} type="checkbox" value="Willing"/> Dont Work in an auxilary at my church, BUT I would love to start
+
+                        <br/>
+                        
+
+                	</fieldset>
+
+                	  <button className='btn btn-primary' type="submit" value="Signup" > Signup </button>
+                	
+                	</form>
+                </div>
+               </div>
+            )
+}
+}
 
 import {connect} from 'react-redux'
 
