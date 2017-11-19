6c38ac0eba2d62d811285d6499f7318c2baba758
Khristian Brooks <khristian@meetup.com>
2017-11-19 01:05:45 -0500

    Updated Nav to use react-bootstrap, added carousel to homepage

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import { Carousel, CarouselItem } from 'react-bootstrap';
 
-export const Home = () => (
-   <div className="home fullscreen-bg">
-   	<Navbar/>
-   </div>
-)
-
-import {connect} from 'react-redux'
-
-export default connect ()(Home)
+export default () => (
+	<div className="">
+		<Navbar />
+		<Carousel>
+			<CarouselItem>
+				<img src="/images/iyc2.png" />
+			</CarouselItem>
+			<CarouselItem>
+				<img src="/images/iyc3.png" />
+			</CarouselItem>
+		</Carousel>
+	</div>
+);
