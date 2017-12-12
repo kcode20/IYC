45a98112e236718b2d75b32958ed75aae590247b
Khristian Brooks <kh.brooks02@gmail.com>
2017-12-12 18:47:01 -0500

    Finished adding form fields

--- app/components/YouthRegistration.jsx
+++ app/components/YouthRegistration.jsx
@@ -27,6 +27,20 @@ export class YouthRegistration extends React.Component {
 			<form>
 				<Grid>
 					<Col xs={6} md={4}>
+						Event Details
+					</Col>
+					<Col xs={12} md={8}>
+						<FormControl.Static>
+							Youth Leader’s Summit (formerly known as the Youth Leader’s
+							Meeting) We’re so excited about the International Youth Conference
+							2018! This year Youth Workers, Youth Leaders, Ministers, Pastors,
+							and other attendees are asked to register for this year’s “Youth
+							Leader’s Summit”. There is a registration price of $10. A portion
+							of the registration proceeds will be sent to help aid in the
+							recovery efforts in the Virgin Islands.
+						</FormControl.Static>
+					</Col>
+					<Col xs={6} md={4}>
 						Personal Information
 					</Col>
 					<Col xs={12} md={8}>
@@ -125,6 +139,82 @@ export class YouthRegistration extends React.Component {
 							<Field
 								name="lead"
 								placeholder="#"
+								component={FormControl}
+								type="text"
+							/>
+						</div>
+						<div>
+							<ControlLabel>What auxiliaries do you have?</ControlLabel>
+							<Field
+								name="auxilary"
+								placeholder="Orchestra, Choir, Outreach"
+								component={FormControl}
+								type="text"
+							/>
+						</div>
+					</Col>
+					<Col xs={6} md={4}>
+						Profile Questions
+					</Col>
+					<Col xs={12} md={8}>
+						<div>
+							<ControlLabel>
+								List three strengths you as a youth leader possess.
+							</ControlLabel>
+							<Field
+								name="strengths"
+								componentClass="textarea"
+								component={FormControl}
+								type="text"
+							/>
+						</div>
+						<div>
+							<ControlLabel>
+								List three things you wish to improve upon as a youth leader.
+							</ControlLabel>
+							<Field
+								name="improvements"
+								componentClass="textarea"
+								component={FormControl}
+								type="text"
+							/>
+						</div>
+						<div>
+							<ControlLabel>Did you attend IYC 2017?</ControlLabel>
+							<Field
+								name="attendance"
+								component={Radio}
+								type="radio"
+								value="Yes"
+							/>
+							Yes
+							<Field
+								name="attendance"
+								component={Radio}
+								type="radio"
+								value="No"
+							/>
+							No
+						</div>
+						<div>
+							<ControlLabel>
+								What was one thing you enjoyed about IYC 2017?
+							</ControlLabel>
+							<Field
+								name="enjoy_iyc"
+								componentClass="textarea"
+								component={FormControl}
+								type="text"
+							/>
+						</div>
+						<div>
+							<ControlLabel>
+								If you could suggest one thing to the IYC Committee for 2018
+								what would it be?
+							</ControlLabel>
+							<Field
+								name="suggestions_iyc"
+								componentClass="textarea"
 								component={FormControl}
 								type="text"
 							/>
